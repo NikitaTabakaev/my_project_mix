92236fbc67db7850592ff45e24750f0ac0027896
unknown <nikita-qp@yandex.ru>
2021-06-12 14:41:42 +0300

    new_version

--- dark_side.py
+++ dark_side.py
@@ -6,12 +6,13 @@ class Tobacco:
     def __init__(self, brand, strength):
         self.brand = brand
         self.strength = strength
-        self.fresh_taste = ["Супернова", "Лимон", "Цитрусовый микс", "Лайм"]
-        self.berry_taste = ["Лесные ягоды", "Ежевика", "Черная смородина", "Красная смородина", "Черника",
-                            "Овсянка с ягодами", "Малина", "Клубника"]
-        self.fruit_taste = ["Грейпфрут", "Пина каладо", "Апельсин", "Сливочный кокос", "Лайм", "Гуава", "Гранат"]
-        self.desert_taste = ["Чизкейк", "Тирамису", "Пряник", "Шоколадное печенье", "Шоколадное мороженное",
-                             "Сливочный кокос", "Клубничный джем"]
+        self.fresh_taste = []
+        self.berry_taste = []
+        self.fruit_taste = []
+        self.desert_taste = []
+
+    def info_tobacco(self):
+        return f"Название - {self.brand} \nКрепость - {self.strength}"
 
     def get_mix(self, first, second, fresh=""):
 
@@ -32,7 +33,7 @@ class Tobacco:
             choice_2 = random.choice(self.desert_taste)
 
         if choice_1 != choice_2 and fresh == "да":
-            return f"Твой микс готов!: {choice_1} + {choice_2} и {random.choice(self.fresh_taste)}"
+            return f"Твой микс готов!: {choice_1} + {choice_2} + {random.choice(self.fresh_taste)}"
         elif choice_1 != choice_2:
             return f"Твой микс готов!: {choice_1} + {choice_2}"
 
@@ -46,13 +47,33 @@ class Tobacco:
             if choice_1 != choice_2:
                 return f"Твой микс готов!: {choice_1} + {choice_2}"
 
+class DarkSide(Tobacco):
 
-tobacco_1 = Tobacco("Дарк Сайд", "Средний")
+    def __init__(self, brand, strength):
+        super().__init__(brand, strength)
+        self.fresh_taste = ["Супернова", "Лимон", "Цитрусовый микс", "Лайм"]
+        self.berry_taste = ["Лесные ягоды", "Ежевика", "Черная смородина", "Красная смородина", "Черника",
+                            "Овсянка с ягодами", "Малина", "Клубника"]
+        self.fruit_taste = ["Грейпфрут", "Пина каладо", "Апельсин", "Сливочный кокос", "Лайм", "Гуава", "Гранат"]
+        self.desert_taste = ["Чизкейк", "Тирамису", "Пряник", "Шоколадное печенье", "Шоколадное мороженное",
+                             "Сливочный кокос", "Клубничный джем"]
 
-q_1 = input("Привет! я помогу тебе подобрать микс! первый вкус: ягоды, фрукты, свежесть, десерты?: ").lower()
-q_2 = input("Что добавить?: ягоды, фрукты, свежесть, десерты?: ").lower()
-q_3 = input("Добавить свежести?: ").lower()
-print(tobacco_1.get_mix(q_1, q_2, q_3))
+
+tobacco_1 = DarkSide("Дарк Сайд", "Средний")
+
+while True:
+    if __name__ == "__main__":
+        q_1 = input("первый вкус: ягоды, фрукты, свежесть, десерты?: ").lower()
+        q_2 = input("второй: ягоды, фрукты, свежесть, десерты?: ").lower()
+        q_3 = input("Добавить свежести?: ").lower()
+        print(tobacco_1.get_mix(q_1, q_2, q_3))
+
+        q_4 = input("Завершить программу?: ")
+        if q_4 == "да":
+            break
+
+
+
 
 
 
